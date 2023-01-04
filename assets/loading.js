adfc3f8700b769935576c08d491d8a4fa9426fb3
Taufik Akbar Maliki <taufikakbarmalikitkj@gmail.com>
2023-01-04 15:48:00 +0800

    update new bg

---
+++ assets/loading.js
@@ -0,0 +1,8 @@
+{
+    "apiKey": "AIzaSyCmRzJqD28EwB26YvKyLo07V7DxzxR08J8",
+    "authDomain": "raker-5a160.firebaseapp.com",
+    "projectId": "raker-5a160",
+    "storageBucket": "raker-5a160.appspot.com",
+    "messagingSenderId": "1073917849528",
+    "appId": "1:1073917849528:web:07d3551851513bb16ca94d"
+}
